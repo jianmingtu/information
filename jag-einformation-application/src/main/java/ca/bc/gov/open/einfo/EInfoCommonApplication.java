5aff88cde7193dffecf741c6a33e49a168bc9670
jianming tu <tu_jm@yahoo.com>
2023-03-20 23:54:04 -0700

    init

---
+++ jag-einformation-application/src/main/java/ca/bc/gov/open/einfo/EInfoCommonApplication.java
@@ -0,0 +1,11 @@
+package ca.bc.gov.open.einfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EInfoCommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EInfoCommonApplication.class, args);
+    }
+}
